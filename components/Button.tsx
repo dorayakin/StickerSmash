a19a90bb7592199accfcbf48088ce6c202f390b7
dorayakin <42695876+dorayakin@users.noreply.github.com>
2025-10-09 00:09:52 +0900

    Refactor `Button` component: fix import path, add theme handling, and improve styling with icon spacing and label color adjustments. Update usage in `index.tsx`.
    Chapter 3 complete.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import {Pressable, StyleSheet, View, Text} from 'react-native';
-import {FontAwesome} from "@expo/vector-icons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Props = {
     label: string;
@@ -8,13 +8,15 @@ type Props = {
 
 export default function Button({label, theme}: Props) {
     if (theme === 'primary') {
-        return (<View style={[styles.buttonContainer, {
+        return (
+            <View style={[styles.buttonContainer, {
                 borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18
-            },]}>
+            },
+            ]}>
                 <Pressable style={[styles.button, {backgroundColor: '#fff'}]}
                            onPress={() => alert('You pressed a button.')}>
-                    <FontAwesome name="picture-o" size={18} color={"#25292e"} style={styles.button}/>
-
+                    <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
+                    <Text style={[styles.buttonLabel, {color: '#25295e'}]}>{label}</Text>
                 </Pressable>
             </View>
 
@@ -48,6 +50,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
+    },
+    buttonIcon: {
+        paddingRight: 8,
     },
     buttonLabel: {
         color: '#fff',
