fcfaac37d71e4ef556c97687ae14548ef658413e
dorayakin <42695876+dorayakin@users.noreply.github.com>
2025-10-10 00:22:32 +0900

    Add `onPress` prop to `Button` and integrate `expo-image-picker` for photo selection. Update dependencies.
    Chapter 3-3　complete.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,23 @@ import {View, StyleSheet} from "react-native";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
-
-
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1,
+        });
+        if (!result.canceled) {
+            console.log(result);
+        } else {
+            alert('You did not select any image.');
+        }
+    };
     return (
         <View
             style={styles.container}>
@@ -15,8 +26,8 @@ export default function Index() {
                 <ImageViewer imgSource={PlaceholderImage}/>
             </View>
             <View style={styles.footerContainer}>
-                <Button theme="primary" label="Choose a photo" />
-                <Button label="Use this photo" />
+                <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+                <Button label="Use this photo"/>
             </View>
         </View>
     );
@@ -36,7 +47,7 @@ const styles = StyleSheet.create({
         height: 440,
         borderRadius: 18
     },
-    footerContainer:{
+    footerContainer: {
         flex: 1 / 3,
         alignItems: "center",
     },
